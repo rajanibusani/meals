af2bbb82eb68b7100baf8e159847120a06704f47
Rajani <brajanipharma@gmail.com>
2023-11-02 23:02:16 +0100

    updated all the dependencies( node, webpack..)

--- database_dump_meals.sql
+++ database_dump_meals.sql
@@ -1,23 +1,55 @@
-CREATE DATABASE  IF NOT EXISTS `meal-sharing`;
-USE `meal-sharing`;
---
--- Table structure for table `meals`
---
+CREATE SCHEMA meal-sharing;
+USE meal-sharing;
+CREATE TABLE meals(id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+				  title VARCHAR(255) NULL,
+				 description TEXT NULL,
+                 location VARCHAR(255) NULL,
+                 `when` DATETIME NULL,
+                 max_reservations INT NULL,
+                 price DECIMAL NULL,
+                 created_date DATETIME DEFAULT CURRENT_TIMESTAMP);
+-- Creating table Reservation
+CREATE TABLE reservations(id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+						 number_of_guests INT NULL,
+                         meal_id INT(11) UNSIGNED NOT NULL,
+                         created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+                         contact_phonenumber VARCHAR(30) NULL,
+                         contact_name VARCHAR(255) NULL,
+                         contact_email VARCHAR(255) NULL,
+                         FOREIGN KEY (meal_id) REFERENCES meals(id) ON DELETE CASCADE);
+-- Creating table review
+CREATE TABLE reviews(id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+				    title VARCHAR(255) NULL,
+                    description TEXT NULL,
+                    meal_id INT(11) UNSIGNED NOT NULL,
+                    stars INT(11) NULL,
+                    created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    FOREIGN KEY (meal_id) REFERENCES meals(id) ON DELETE CASCADE);
+INSERT INTO meals (id,title, description,location, `when`, max_reservations, price)
+ VALUES(1,'Italian Pasta With Dessert', 'Spinach ravioli with a fresh tomato sauce and Tiramisu','Valby','2021-06-02 19:30:00',20,150),
+ (2,'Chicken stew with appam','Chicken stew with south indian pancake', 'Bagsværd', '2021-05-29 09:00:00', 25, 75),
+ (3,'Mexican Shrimp Tacos','Crispy cheesy shrimp tacos and homemade salsa', 'Brøndby', '2021-06-14 18:30:00', 20, 125), 
+ (4,'Baklava and Sfouf Cake','Baklava which is pastry layered with chopped nuts and sweetened with syrup, Sfouf Cake which is almond-semolina cake', 'Hedehusene', '2021-05-30 17:00:00', 15, 90),
+ (5,'Risotto with shrimp','Risotto with shrimp lemon and rosemary', 'Ballerup', '2021-06-20 19:30:00', 25, 250),
+ (6,'SouthIndian Breakfast', 'Dosa (made with lentils and rice batter) with Peanut sause','Søborg','2021-06-14 08:30:00', 120, 25),
+  (7,'Steamed Momos & Choyla','Steamed Momos, one of the most popular dishes in Nepal Choyla, a traditional Nepali spiced meat dish', 'København','2021-06-14 18:30:00', 25, 170),
+  (8,'Kadai Paneer with Naan','Indian curry made with paneer (cottage cheese), tomato, butter and cashew sauce and with Indian flatbread', 'hvidovre', '2021-07-02 19:30:00', 25, 220);  
 
-DROP TABLE IF EXISTS `meals`;
-CREATE TABLE `meals` (
-  `idmeals` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) DEFAULT NULL,
-  `number_of_guests` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idmeals`)
-) ENGINE=InnoDB AUTO_INCREMENT=6;
+INSERT INTO reservations (id,number_of_guests,meal_id,contact_phonenumber,contact_name,contact_email)
+VALUES(1,4,1,'12 43 65 78', 'Mette','mette@gmail.com'),
+  (2,5,3,'28 74 93 74', 'Chris','chris@gmail.com')
+(3,6,2,'98 34 73 74', 'katie','katie@gmail.com'),
+(4,5,6,'22 72 23 14', 'Marianna','mas@gmail.com'),
+(5,8,3,'18 14 33 24', 'Daniel','daniel@gmail.com'),
+(6,7,3,'21 12 83 14', 'Lucy','lucy@gmail.com'),
+(7,7,5,'18 14 23 24', 'Jade Parker','jade@gmail.com') ; 
 
---
--- Dumping data for table `meals`
---
 
-LOCK TABLES `meals` WRITE;
-/*!40000 ALTER TABLE `meals` DISABLE KEYS */;
-INSERT INTO `meals` VALUES (1,'bla bla bla',3),(2,'bla bla bla',3),(3,'asdasd',7),(4,'benjamins karry',10),(5,'oooooooooo',1);
-/*!40000 ALTER TABLE `meals` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO reviews (id,title,description,meal_id,stars)
+ VALUES
+(1,'Spinach ravioli','It was very delicious',1,4),
+(2,'Chicken stew With appam','Delicious breakfast',2,5),
+(3,'Tacos','very authentic, tasty',3,3),
+(4,'Dosa','Tried for first time, loved the taste of it',6,5),
+(5,'paneer with naan','So nice',8,4),
+(6,'Tacos','Enjoyed so much',3,5);
